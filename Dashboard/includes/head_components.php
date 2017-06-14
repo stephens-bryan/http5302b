1d1f9d88039d9305dd5a5341f7e068d75b4d91d5
Julia Lim <juliamlim@hotmail.com>
2017-06-14 13:07:28 -0400

    Admin Dashboard
    Not sure how we want files set up yet

---
+++ Dashboard/includes/head_components.php
@@ -0,0 +1,10 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Materialize -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+<link rel="stylesheet" href="admin_style.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
