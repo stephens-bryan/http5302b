2bc2af96cbffc62216cd7dec230eb8f32c448af3
bryanstephens <bryanstephensdevelopment@gmail.com>
2017-06-19 19:25:49 +0000

    added AccountTest.php

--- AccountTest.php
+++ AccountTest.php
@@ -1,1 +1,15 @@
+<? php
+require "vendor/autoload.php"
 
+ /*
+ * @covers Account
+ */
+  
+  final class AccountTest extends TestCase{
+    
+    /*
+    return True if user can 
+    */
+  }
+  
+?>
