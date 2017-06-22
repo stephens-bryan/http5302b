3991babd2a3bc5d82b7068a2bc6a87a2d1e1f582
Shoby <nahkbiaohs@gmail.com>
2017-06-22 09:44:29 -0400

    Minor edit to classes-delete

--- Dashboard/classes-admin/classes-delete.php
+++ Dashboard/classes-admin/classes-delete.php
@@ -31,15 +31,10 @@
          
     }
 ?>
- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
+
  
 <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <div class="container">
                 <div class="span10 offset1">
                     <div class="row">
