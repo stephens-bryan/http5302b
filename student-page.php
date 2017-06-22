22a6afd505e520b47f8bb922dd2666b8de54f1ef
Josh <jmccormick1024@gmail.com>
2017-06-22 20:03:23 +0000

    fix error on student page

--- student-page.php
+++ student-page.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 3);
 require_once 'includes.php';
 require_once 'database.php';
 //hardcoded student id value for now
-$studentId = $_GET['student'];
+$studentId = 1;
 
 //must grab student profile information
 $student = new Students;
