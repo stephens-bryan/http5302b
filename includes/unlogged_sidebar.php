4257d75427b373e8e7a9a97ddb44c50bbceedf0d
evanpizzolato <evanpizzolato@gmail.com>
2017-06-23 19:18:06 +0000

    added search pages,a dn updated styling to match search fields

--- includes/unlogged_sidebar.php
+++ includes/unlogged_sidebar.php
@@ -1,7 +1,9 @@
 <div>
     <ul class="collapsible side-nav fixed bar-color" id="slide-out" data-collapsible="accordion"> <img class="logo" src="img/humber_logo.png" alt="Humber Logo Image">
         <h1 class="year">2017</h1>
-        <input id="search" type="search" placeholder="Search by language, skill or discipline." />
+      <form action='search.php' method='post' id="search">
+        <input id="searchQuery" type="search" placeholder="Search by language, skill or discipline." />
+      </form>
 <!--         <li>
             <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
             <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
