2f58e92c5d1235d2717cbbb83e1732e6da0907f9
bryanstephens <bryanstephensdevelopment@gmail.com>
2017-06-28 01:56:08 +0000

    fixed merge conflict on config.php

--- config.php
+++ config.php
@@ -1,6 +1,11 @@
 <?php
-
+// define( 'HOST', 'portfolio.cga94bd83uty.ca-central-1.rds.amazonaws.com' );                                                                                                        
+// define( 'PORT', '3306' );                                                                                                                                                         
+// define( 'USERNAME', 'teammember' );                                                                                                                                               
+// define( 'PASSWORD', 'phpteam1!' );                                                                                                                                                
+// define( 'DATABASE', 'portfolio' );                                                                                                                                                
+    
 define( 'HOST', 'localhost' );
 define( 'USERNAME', 'root' );
-define( 'PASSWORD', '' );
+define( 'PASSWORD', 'password' );
 define( 'DATABASE', 'portfolio' );
