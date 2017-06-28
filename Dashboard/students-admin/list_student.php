1ce5e65c6281cf4a82bc3e82223d5e0758ce01fb
ericsmith92 <ericsmth884@gmail.com>
2017-06-27 21:55:32 -0400

    Update list_student.php
    added search

--- Dashboard/students-admin/list_student.php
+++ Dashboard/students-admin/list_student.php
@@ -10,12 +10,76 @@ require_once "../includes/admin_nav.php";
 
 //require header
 require_once "./includes/header.php";
+//require search class
+require_once "../../functions/SearchFunctions/search.php";
 
+    //call list students on student object
     $listStudents = $student->getStudents();
+
+    //if search has been submitted, store results for output
+    if(isset($_GET['search'])){
+        //store search term
+        $searchTerm = $_GET['searchTerm'];
+        //new search object
+        $searchObj = new Search();
+        $searchResults = $searchObj->searchStudents($db, $searchTerm);
+    }
+
 
 ?>
 
-<h3>List Students</h3>
+<div>Search Students</div>
+    <form method="get" action="<?= $_SERVER['PHP_SELF']; ?>">
+        <input type="text" name="searchTerm">
+        <input type="submit" name="search" value="search">
+    </form>
+
+<?php if(isset($searchResults)){foreach($searchResults as $results) {
+
+    echo "<table>" .
+            "<thead>" .
+                "<tr>" .
+                    "<th>" . 'Id' . "</th>" .
+                    "<th>" . 'First Name' . "</th>" .
+                    "<th>" . 'Last Name' . "</th>" .
+                    "<th>" . 'Student Number' . "</th>" .
+                    "<th>" . 'Contact Email' . "</th>" .
+                    "<th>" . 'Last Update' . "</th>" .
+                    "<th>" . 'Contact Number' . "</th>" .
+                    "<th>" . 'Current Job' . "</th>" .
+                    "<th>" . 'Edit' . "</th>" .
+                    "<th>" . 'Delete' . "</th>" .
+                "</tr>" .
+            "</thead>" .
+            "<tbody>";
+
+
+    ?>
+    <tr>
+        <td><?= $results["Id"]; ?></td>
+        <td><?= $results["FirstName"]; ?></td>
+        <td><?= $results["LastName"]; ?></td>
+        <td><?= $results["StudentNumber"]; ?></td>
+        <td><?= $results["ContactEmail"]; ?></td>
+        <td><?= $results["LastUpdate"]; ?></td>
+        <td><?= $results["ContactNumber"]; ?></td>
+        <td><?= $results["CurrentJob"]; ?></td>
+        <!-- Update Student-->
+        <td><form action="update_student.php" method="post">
+                <input type="hidden" name="id" value="<?= $results["Id"]; ?>">
+                <input type="submit" name="submit" value="EDIT" class="btn">
+            </form></td>
+        <!-- Delete Student -->
+        <td><form action="delete_student.php" method="post">
+                <input type="hidden" name="id" value="<?= $results["Id"]; ?>">
+                <input type="submit" name="submit" value="DELETE" class="btn">
+            </form></td>
+    </tr>
+    </tbody>
+    </table>
+
+    <?php }}?>
+<h1>List Students</h1>
 
     <a href="add_student.php">Add Student</a>
 
