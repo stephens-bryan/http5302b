cf45fd3d9a1ef3664d84e240a253f4af191eda35
bryanstephens <bryanstephensdevelopment@gmail.com>
2017-06-28 01:50:24 +0000

    fixed config.php.

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
 
+// define( 'HOST', 'portfolio.cga94bd83uty.ca-central-1.rds.amazonaws.com' );
+// define( 'PORT', '3306' );
+// define( 'USERNAME', 'teammember' );
+// define( 'PASSWORD', 'phpteam1!' );
+// define( 'DATABASE', 'portfolio' );
 
-define( 'HOST', '127.0.0.1' );
+define( 'HOST', 'localhost' );
 define( 'USERNAME', 'root' );
-define( 'PASSWORD', 'root' );
+define( 'PASSWORD', '' );
 define( 'DATABASE', 'portfolio' );
